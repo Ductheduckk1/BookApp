bc2b92d023b99e6a7f5270faaa8d9da61dea452e
DucTheDuckk1 <a11duc1911@gmail.com>
2025-11-01 14:03:26 +0700

    init commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,15 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kapt)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.realm.android)
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 android {
     namespace = "com.example.bookapp"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.bookapp"
@@ -34,24 +36,71 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
 }
 
 dependencies {
-
+    implementation("androidx.core:core-splashscreen:1.0.1")
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    api(libs.mavericks)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.preference.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.javax.inject)
+    implementation(libs.flowbinding.android)
+    implementation(libs.dagger)
+    kapt(libs.dagger.compiler)
+    implementation(libs.hilt)
+    kapt(libs.hilt.compiler)
+    androidTestImplementation(libs.hilt.android.testing)
+    implementation(libs.realm.monarchy)
+    implementation(platform(libs.okhttp.bom))
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+    implementation(libs.moshi)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.moshi)
+    implementation(libs.moshi.kt)
+    implementation(libs.moshi.adapters)
+    kapt(libs.moshi.kotlin.codegen)
+    implementation(libs.epoxy)
+    implementation(libs.epoxy.glide)
+    kapt(libs.epoxy.processor)
+    implementation(libs.epoxy.paging)
+    implementation(libs.android.map.sdk)
+    implementation(libs.android.sdk.geojson)
+    implementation(libs.gms.play.services.location)
+    implementation(libs.graphhopper.core)
+    implementation(libs.glide)
+    implementation(libs.blurry)
+
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.exoplayer.rtsp)
+    implementation(libs.androidx.media3.ui)
+    implementation (libs.rtsp.server)
+    implementation(libs.library)
+
+    implementation(libs.camera.core)
+    implementation(libs.camera.camera2)
+    implementation(libs.camera.lifecycle)
+    implementation(libs.camera.view)
+}
+
+hilt {
+    enableAggregatingTask = false
+}
+
+kapt {
+    correctErrorTypes = true
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,68 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ktlint)
+}
+
+buildscript {
+    repositories {
+        // Do not use `mavenCentral()`, it prevents Dependabot from working properly
+        maven {
+            url = uri("https://repo1.maven.org/maven2")
+            url = uri("https://jitpack.io")
+        }
+    }
+    dependencies {
+        classpath( libs.realm.gradle.plugin)
+    }
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions.allWarningsAsErrors =
+            project.findProperty("allWarningsAsErrors")?.toString()?.toBoolean() ?: true
+    }
+
+    tasks.withType<org.jlleitschuh.gradle.ktlint.tasks.BaseKtLintCheckTask>().configureEach {
+        workerMaxHeapSize.set("2G")
+    }
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        version.set("0.45.1")
+        android.set(true)
+        ignoreFailures.set(false)
+        enableExperimentalRules.set(true)
+        verbose.set(true)
+
+        reporters {
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.PLAIN)
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
+        }
+
+        filter {
+            exclude { element -> element.file.path.contains("$buildDir/generated/") }
+        }
+
+        disabledRules.set(
+            listOf(
+                "indent",
+                "experimental:argument-list-wrapping",
+                "max-line-length",
+                "parameter-list-wrapping",
+                "spacing-between-declarations-with-comments",
+                "no-multi-spaces",
+                "experimental:spacing-between-declarations-with-annotations",
+                "experimental:annotation",
+                "wrapping",
+                "experimental:trailing-comma",
+                "experimental:comment-wrapping",
+                "experimental:kdoc-wrapping",
+                "string-template"
+            )
+        )
+    }
 }
